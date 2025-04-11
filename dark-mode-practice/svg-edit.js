3b9c75c52157b23d8f733ee25cd34f44195e5e5b
NathanaelLoop <Jinderloop@gmail.com>
2025-04-11 18:29:33 -0400

    "Dark mode completao"

---
+++ dark-mode-practice/svg-edit.js
@@ -0,0 +1,7 @@
+let buttonDM = document.querySelector('#buttonDM');
+
+buttonDM.addEventListener("click", switchDarkMode);
+
+function switchDarkMode(){
+    document.documentElement.classList.toggle("dark")
+};
